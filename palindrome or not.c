9030b4ed61ffb5373b7af423f55e7e0b14a1bdca
Vijilee George Kurian <vijileegk2006@gmail.com>
2025-02-02 12:02:57 +0530

    Add files via upload

---
+++ palindrome or not.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num,unmodifed_num,reversed_num=0,remainder;
+    printf("Enter some number:");
+    scanf("%d",&num);
+    unmodifed_num=num;
+    while(num!=0)
+    {
+        remainder=num%10;
+        reversed_num=reversed_num*10+remainder;
+        num=num/10;
+        
+    }
+    printf("%d is the reversed form",reversed_num);
+    if (unmodifed_num==reversed_num)
+    {
+        printf("\nIt is palindrome");
+    }
+    else{
+        printf("\nIt's not palindrome");
+    }
+    return 0;
+    
+}

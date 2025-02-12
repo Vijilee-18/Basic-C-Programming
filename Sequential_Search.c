b9f9732b30080ecf91b2ed72e35daac59eb82361
Vijilee George Kurian <vijileegk2006@gmail.com>
2025-02-12 18:36:26 +0530

    Add files via upload

---
+++ Sequential_Search.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,limit,key,flag=0,pos;
+    printf("Enter the size of array: ");
+    scanf("%d",&limit);
+    int array[limit];
+    for(i=0;i<limit;i++)
+    {
+        printf("Enter the values:");
+        scanf("\n%d",&array[i]);
+    }
+    printf("enter the value to be founded:");
+    scanf("%d",&key);
+    for(i=0;i<limit;i++)
+    {
+        if(array[i]==key)
+        {
+            flag=1;
+            pos=i;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("%d has been founded at the index number %d",key,pos);
+    }
+    else
+    {
+        printf("%d is not founded in the array",key);
+    }
+    return 0;
+}

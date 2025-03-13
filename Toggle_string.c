f0a461ac8dbf73be323fffebd5cdaca32ea53aa8
Vijilee George Kurian <vijileegk2006@gmail.com>
2025-03-13 21:32:50 +0530

    Add files via upload

---
+++ Toggle_string.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    printf("Enter the string:");
+    scanf("%[^\n]",str);
+    int i;
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='a'&& str[i]<'z')
+        {
+            str[i]=str[i]-32;
+        }
+        else if(str[i]>='A' && str[i]<'Z')
+        {
+            str[i]=str[i]+32;
+        }
+
+    }
+    printf("%s is the toggled string",str);
+    return 0;
+}
